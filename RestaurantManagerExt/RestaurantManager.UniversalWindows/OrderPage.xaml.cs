0d9ec525a69ec42521606c332aa2a3ba599940b8
Jan Wild <jan.wild@hotmail.com>
2016-05-04 12:43:48 +0200

    Finally added extended solution?

---
+++ RestaurantManagerExt/RestaurantManager.UniversalWindows/OrderPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace RestaurantManager.UniversalWindows
+{
+    /// <summary>
+    /// An Order page that manages orders.
+    /// </summary>
+    public sealed partial class OrderPage : Page
+    {
+        public OrderPage()
+        {
+            this.InitializeComponent();
+        }
+        /// <summary>
+        /// Navigates back to the MainPage
+        /// </summary>
+        /// <param name="sender">btnHomeOrder</param>
+        /// <param name="e">Click</param>
+        private void btnHomeOrder_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
+    }
+}
