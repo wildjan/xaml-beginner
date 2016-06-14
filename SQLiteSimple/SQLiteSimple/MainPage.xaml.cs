ba7bfdb9cf7f6e3b8b99e1d99aff2da5812e6d2c
jwildNB <Jan.Wild@hotmail.com>
2016-06-14 14:14:11 +0200

    Startin code

--- SQLiteSimple/SQLiteSimple/MainPage.xaml.cs
+++ SQLiteSimple/SQLiteSimple/MainPage.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using SQLite;
+using System.Collections.ObjectModel;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using System;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +15,58 @@ namespace SQLiteSimple
         public MainPage()
         {
             this.InitializeComponent();
+
+            SQLiteConnection DB = new SQLiteConnection("peopleDB.sqlite");
+
+            DB.CreateTable<Person>();
+
+            int countPeople = DB.Table<Person>().Count();
+
+            // if the DB has information, start app with that data
+            if (countPeople >= 1)
+            {
+                DBtoList();
+                DisplayDB();
+            }
+            // if the DB is empty, initialize the collection and load that info
+            else
+            {
+                initPeopleCollection();
+                loadCollection();
+            }
+        }
+
+        private void DBtoList()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DisplayDB()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void loadCollection()
+        {
+            throw new NotImplementedException();
+        }
+
+        ObservableCollection<Person> people = new ObservableCollection<Person>();
+
+        public class Person
+        {
+            [PrimaryKey]
+            public int ID { get; set; }
+            public string Name { get; set; }
+        }
+
+        private void initPeopleCollection()
+        {
+            people.Clear();
+
+            people.Add(new Person() { ID = 0, Name = "Chris" });
+            people.Add(new Person() { ID = 1, Name = "Sage" });
+            people.Add(new Person() { ID = 2, Name = "Daren" });
         }
     }
 }
